8d7176b6c79eed44a92d86a4b51f869c7959ec7a
novoseltcev <novoseltcev.stanislav@gmail.com>
2022-11-02 21:59:45 +0300

    fix(hooks): Invalid error proccessing

--- hooks/check_msg.py
+++ hooks/check_msg.py
@@ -56,11 +56,8 @@ def main():
         validate_commit(message, get_branch_name())
         print("Message validated")
         sys.exit(0)
-    except TypeError as e:
-        print(e.args[1], file=sys.stderr)
-        sys.exit(1)
     except Exception as e:
-        print(e.args[0], file=sys.stderr)
+        print(e.args[-1], file=sys.stderr)
         sys.exit(1)
 
 
